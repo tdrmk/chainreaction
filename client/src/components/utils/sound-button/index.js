0dff91f16f6113486ec9eae4f10eceebcc18ae4d
Dinesh Ram Kumar Thandapani <tdrk@uber.com>
2021-09-05 00:55:08 +0530

    persist sound preference, and custom event on change

--- client/src/components/utils/sound-button/index.js
+++ client/src/components/utils/sound-button/index.js
@@ -1,3 +1,4 @@
+import { ignoreErr } from "../../../utils/error";
 import { createTemplate } from "../../../utils/shadowdom";
 import htmlcontents from "./index.html";
 
@@ -8,14 +9,18 @@ const STATE = {
   OFF: "OFF",
 };
 
+const SOUND_PREFERENCE = "sound_preference";
 class SoundButton extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
 
-    // state
-    this.state = STATE.ON;
+    // load from local storage
+    const savedstate = ignoreErr(() => localStorage.getItem(SOUND_PREFERENCE));
+    this.state = Object.values(STATE).includes(savedstate)
+      ? savedstate
+      : STATE.ON;
 
     const onbtn = this.shadowRoot.querySelector("#volume-on");
     const offbtn = this.shadowRoot.querySelector("#volume-off");
@@ -23,18 +28,32 @@ class SoundButton extends HTMLElement {
     onbtn.style.display = this.state === STATE.ON ? "block" : "none";
     offbtn.style.display = this.state === STATE.ON ? "none" : "block";
 
+    const onupdate = () => {
+      this.dispatchEvent(
+        new CustomEvent("volumechange", { detail: this.state })
+      );
+
+      // persist sound preference
+      ignoreErr(() => localStorage.setItem(SOUND_PREFERENCE, this.state));
+    };
+
     // event handlers
     onbtn.addEventListener("click", (event) => {
       this.state = STATE.OFF;
       onbtn.style.display = "none";
       offbtn.style.display = "block";
+      onupdate();
     });
 
     offbtn.addEventListener("click", (event) => {
       this.state = STATE.ON;
       onbtn.style.display = "block";
       offbtn.style.display = "none";
+      onupdate();
     });
+
+    // notify initial state
+    onupdate();
   }
 
   get enabled() {
